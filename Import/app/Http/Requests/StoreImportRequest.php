b6ebfea27d7bf172f65594fbccf9f4f1afbf0062
admin <admin@admins-MacBook-Pro.local>
2025-09-12 11:30:26 +0300

    prepare validation hash rule

--- Import/app/Http/Requests/StoreImportRequest.php
+++ Import/app/Http/Requests/StoreImportRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\ImportHashNotInProgress;
 
 class StoreImportRequest extends FormRequest
 {
@@ -14,7 +15,12 @@ class StoreImportRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'file' => 'required|file|max:10240'
+            'file' => [
+                'required',
+                'file',
+                'max:10240',
+                new ImportHashNotInProgress(),
+            ],
         ];
     }
 }
