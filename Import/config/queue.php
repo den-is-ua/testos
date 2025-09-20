77433aba3079697b4f3181e1aaa09036eb50ed25
admin <admin@admins-MacBook-Pro.local>
2025-09-20 07:29:44 +0300

    fix configs

--- Import/config/queue.php
+++ Import/config/queue.php
@@ -73,6 +73,15 @@ return [
         ],
         'rabbitmq' => [
             'driver' => 'rabbitmq',
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+            ],
             'queue'  => env('RABBITMQ_QUEUE', 'default'),
             'connection' => PhpAmqpLib\Connection\AMQPStreamConnection::class,
             'options' => [
